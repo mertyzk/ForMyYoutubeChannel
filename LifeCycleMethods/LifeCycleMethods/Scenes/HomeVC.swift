7d208443e34fe084b1062b2dfcda826a94c060e9
mertyzk <meyuzak@gmail.com>
2023-08-25 14:55:29 +0300

    Lifecycle methods project created

---
+++ LifeCycleMethods/LifeCycleMethods/Scenes/HomeVC.swift
@@ -0,0 +1,12 @@
+//
+//  HomeVC.swift
+//  LifeCycleMethods
+//
+//  Created by Macbook Air on 25.08.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
---
+++ LifeCycleMethods/LifeCycleMethods/Scenes/HomeView.swift
@@ -0,0 +1,12 @@
+//
+//  HomeView.swift
+//  LifeCycleMethods
+//
+//  Created by Macbook Air on 25.08.2023.
+//
+
+import UIKit
+
+class HomeView: UIView {
+    
+}
