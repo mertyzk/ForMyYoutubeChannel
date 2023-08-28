f127abb3f1ba9d4503d02d0d697e2b7c8d128dc1
mertyzk <meyuzak@gmail.com>
2023-08-28 14:50:08 +0300

    about loadView

--- LifeCycleMethods/LifeCycleMethods/Scenes/HomeVC.swift
+++ LifeCycleMethods/LifeCycleMethods/Scenes/HomeVC.swift
@@ -9,4 +9,29 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    let homeView = HomeView()
+    
+    
+    
+    override func loadView() {
+        print("xxxxxxxxxxx DEBUG: loadView")
+        view = homeView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureActions()
+    }
+    
+    
+    // MARK: - Helper Functions
+    private func configureActions() {
+        homeView.testButton.addTarget(self, action: #selector(testButtonTapped), for: .touchUpInside)
+    }
+    
+    
+    @objc func testButtonTapped() {
+        print("xxxxxxxxxxx DEBUG: TEST BUTTON TIKLANDI")
+    }
+    
 }
--- LifeCycleMethods/LifeCycleMethods/Scenes/HomeView.swift
+++ LifeCycleMethods/LifeCycleMethods/Scenes/HomeView.swift
@@ -9,4 +9,33 @@ import UIKit
 
 class HomeView: UIView {
     
+    // MARK: - UI Elements
+    lazy var testButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .black
+        return button
+    }()
+    
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Helper Functions
+    private func configureUI() {
+        addSubview(testButton)
+        testButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            testButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            testButton.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
 }
